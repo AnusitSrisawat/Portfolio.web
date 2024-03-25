54314e59cd9e9f31277a896c11c3db24b96e041c
Jimmy <jimmy18778@gmail.com>
2024-03-25 22:05:43 +0700

    .

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -16,7 +16,7 @@ function Details() {
                             setMenu(index);
                             // console.log(index);
                         }}
-                            className={'bg-pink-300 px-4 py-2 rounded-2xl relative z-[1] cursor-pointer hover:bg-pink-400 hover:font-semibold duration-200' + (menu == index ? ' border-2 border-pink-400 font-bold' : ' border-2 border-transparent')}>
+                            className={'bg-pink-300 rounded-2xl relative z-[1] cursor-pointer hover:bg-pink-400 hover:font-semibold duration-200' + (menu == index ? ' border-2 border-pink-400 font-bold px-4 py-3' : ' border-2 border-transparent px-3 py-1')}>
                             {item}
                         </div>
                     ))}
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,11 +8,11 @@ function Profile() {
         <div className='relative w-full lg:w-2/5 h-[80vh] lg:h-full bg-pink-300 rounded-3xl p-5 lg:p-10 flex flex-col gap-5 md:gap-8 justify-start items-center overflow-auto'>
             <div className='font-semibold text-2xl hover:scale-125 cursor-pointer duration-200'>Anusit Srisawat</div>
             <div className='w-full h-auto object-contain'>
-                <img className='hover:scale-110 duration-200 hover:rotate-6 m-auto h-full w-full md:w-2/5 max-w-36 object-contain rounded-xl cursor-pointer' 
-                src=".\images\pf2.jpg" alt="" />
+                <img className='hover:scale-110 duration-200 hover:rotate-6 m-auto h-full w-full md:w-2/5 max-w-36 object-contain rounded-xl cursor-pointer'
+                    src=".\images\pf2.jpg" alt="" />
             </div>
             <div className='flex justify-center items-center gap-5 flex-col'>
-                <div className='flex justify-center items-center gap-2 flex-col'>
+                <div className='w-full flex justify-center items-center gap-2 flex-col'>
                     <div className='uppercase font-semibold cursor-pointer hover:underline duration-200'>about me</div>
                     <div className='text-justify indent-8'>
                         <p>I am interested in data usage. and have the ability and experience to write websites I have adesire to take what I'm passionate about and do to create something new and creative. I am alwayswilling to learn new skills. I work well in a team environment. and listen effectively when solving problems. So I can work well under pressure.</p>
@@ -20,31 +20,37 @@ function Profile() {
                     </div>
                 </div>
                 <Line />
-                
-                <div className='uppercase font-semibold cursor-pointer hover:underline duration-200'>contacts</div>
-                <div className='grid grid-cols-3 gap-2 justify-start items-start break-words w-full md:pl-20 lg:pl-0'>
 
-                    <div className=''>email</div>
-                    <div className='col-span-2'>sranusit44@gmail.com</div>
+                <div className='w-full flex justify-center items-center gap-2 flex-col'>
 
-                    <div className=''>tel</div>
-                    <div className='col-span-2'>099-972-1944</div>
+                    <div className='uppercase font-semibold cursor-pointer hover:underline duration-200'>contacts</div>
+                    <div className='grid grid-cols-3 gap-2 justify-start items-start break-words w-full md:pl-20 lg:pl-0'>
 
-                    <div className=''>address</div>
-                    <div className='col-span-2'>118/302 Buanakarin Bang Kaeo Bang Phli, 10540 Samut Prakan</div>
+                        <div className=''>email</div>
+                        <div className='col-span-2'>sranusit44@gmail.com</div>
 
+                        <div className=''>tel</div>
+                        <div className='col-span-2'>099-972-1944</div>
+
+                        <div className=''>address</div>
+                        <div className='col-span-2'>118/302 Buanakarin Bang Kaeo Bang Phli, 10540 Samut Prakan</div>
+
+                    </div>
                 </div>
                 <Line />
-                
-                <div className='uppercase font-semibold cursor-pointer hover:underline duration-200'>languages</div>
-                <div className='grid grid-cols-3 gap-2 justify-start items-start break-words w-full md:pl-20  lg:pl-0'>
 
-                    <div className=''>Thai</div>
-                    <div className='col-span-2'>Native</div>
+                <div className='w-full flex justify-center items-center gap-2 flex-col'>
 
-                    <div className=''>English</div>
-                    <div className='col-span-2'>Good</div>
+                    <div className='uppercase font-semibold cursor-pointer hover:underline duration-200'>languages</div>
+                    <div className='grid grid-cols-3 gap-2 justify-start items-start break-words w-full md:pl-20  lg:pl-0'>
 
+                        <div className=''>Thai</div>
+                        <div className='col-span-2'>Native</div>
+
+                        <div className=''>English</div>
+                        <div className='col-span-2'>Good</div>
+
+                    </div>
                 </div>
             </div>
         </div>
