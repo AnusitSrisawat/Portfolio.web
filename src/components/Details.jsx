1f123f541a098e64b71dd7914b68ea8c62e07fd7
Jimmy <jimmy18778@gmail.com>
2024-03-25 22:35:50 +0700

    .

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import EducationData from './EducationData'
 import ExperienceData from './ExperienceData'
+import SkillsData from './SkillsData'
 
 function Details() {
     const [menu, setMenu] = useState(1)
     const [arrayData, setArrayData] = useState(["Education", "Experience", "Skills"])
+    const components = [EducationData, ExperienceData, SkillsData];
 
     return (
         <div className='w-full lg:w-3/5 h-[80vh] lg:h-full bg-blue-300 rounded-3xl p-2 md:p-5 lg:p-10 flex flex-col gap-5 justify-start items-center overflow-auto'>
@@ -16,7 +18,8 @@ function Details() {
                             setMenu(index);
                             // console.log(index);
                         }}
-                            className={'bg-pink-300 rounded-2xl relative z-[1] cursor-pointer hover:bg-pink-400 hover:font-semibold duration-200' + (menu == index ? ' border-2 border-pink-400 font-bold px-4 py-3' : ' border-2 border-transparent px-3 py-1')}>
+                            className={'bg-pink-300 rounded-2xl relative z-[1] cursor-pointer hover:bg-pink-400 hover:font-semibold duration-200' + 
+                            (menu == index ? ' border-2 border-pink-400 font-bold px-4 py-3' : ' border-2 border-transparent px-3 py-1')}>
                             {item}
                         </div>
                     ))}
@@ -25,6 +28,7 @@ function Details() {
                 <div className='relative bg-pink-200 w-full h-[90%] rounded-3xl flex flex-col justify-start items-start p-2 md:p-5 lg:p-10 duration-200 overflow-auto'>
                     {menu == 0 && <EducationData />}
                     {menu == 1 && <ExperienceData />}
+                    {React.createElement(components[menu])}
                 </div>
             </div>
         </div >
--- src/components/SkillsData.jsx
+++ src/components/SkillsData.jsx
@@ -1,25 +1,44 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import Line from './Line'
 
 function SkillsData() {
 
+    const [frontEndData, setFrontEndData] = useState(["HTML", "CSS", "Javascript", "vue.js", "react.js", "tailwind"])
+    const [backEndData, setBackEndData] = useState(["MySQL", "Next.js", "Laravel"])
+
     return (
         <div className='w-full h-full flex flex-col gap-3 justify-start items-start rounded-3xl max-md:p-2'>
 
-            <div className='font-semibold text-2xl md:text-3xl'>Education</div>
+            <div className='font-semibold text-2xl md:text-3xl'>Skills</div>
+
             <div className='text-base flex flex-col'>
-                <div className='break-words font-semibold'>
-                    Bachelor's Degree | <span className=''>Thai-Nichi Institute of Technology, Bangkok</span>
+                <div className='text-lg break-words font-semibold'>
+                    FrontEnd
                 </div>
                 <div className='flex flex-col gap-0.5'>
-                    <div className=''>
-                        Computer Engineering
+                    <div className='flex flex-row gap-2'>
+                        {frontEndData.map((item, index) => (
+                            <div key={index} onClick={() => { }} className={''}>
+                                {item}
+                            </div>
+                        ))}
+
                     </div>
-                    <div className='text-xs'>
-                        Class president of Computer Engineering
-                    </div>
-                    <div className='text-xs'>
-                        First class honors ( <span className='font-semibold text-sm'>GPA 3.70</span> )
+                </div>
+            </div>
+
+            <div className='text-base flex flex-col'>
+                <div className='text-lg break-words font-semibold'>
+                    BackEnd
+                </div>
+                <div className='flex flex-col gap-0.5'>
+                    <div className='flex flex-row gap-2'>
+                        {backEndData.map((item, index) => (
+                            <div key={index} onClick={() => { }} className={''}>
+                                {item}
+                            </div>
+                        ))}
+
                     </div>
                 </div>
             </div>
